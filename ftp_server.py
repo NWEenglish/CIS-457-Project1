eef82603faecc5330f501213fd81ce0b7817da8f
DenverDeBoer <32941698+DenverDeBoer@users.noreply.github.com>
2019-10-09 14:05:37 -0400

    Its a start

--- ftp_server.py
+++ ftp_server.py
@@ -1,1 +1,9 @@
 #Port Number: 20
+
+import socket as socket
+
+host = socket.gethostname()     #Hostname of server
+port = 20                       #Port number for FTP
+
+serverSocket = socket.socket()  #instance of socket
+serverSocket.bind((host, port)) #bind host and port
