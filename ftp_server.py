4ddd91e41b96aeeed19be2fc769f669457f4d5b2
DenverDeBoer <32941698+DenverDeBoer@users.noreply.github.com>
2019-10-09 22:21:46 -0400

    Unconfirmed server program

--- ftp_server.py
+++ ftp_server.py
@@ -1,38 +1,42 @@
 #Port Number: 20
-
+import os
+import pickle
 import socket
 import _thread
 
 def thread(server, connect, data):
-    parsed = data.split(" ")
-    if(parsed[0] == "LIST"):
-        print("")
-    elif(parsed[0] == "RETRIEVE"):
+    parsed = data.split(" ")                #Parse input
+    if(parsed[0] == "LIST"):                #If LIST then list files
+        path = os.getcwd()
+        files = os.listdir(path);
+        filesList = pickle.dumps(files)
+        connect.send(filesList)
+    elif(parsed[0] == "RETRIEVE"):          #If RETRIEVE then return file
         file = open(parsed[1], "r")
         chunk = file.read(1024)
         while(chunk):
             connect.send(chunk)
             chunk = file.read(1024)
-    elif(parsed[0] == "STORE"):
+    elif(parsed[0] == "STORE"):             #If STORE then save file
         filename = open(parsed[1], "w")
         while(True):
             file = server.recv(1024).decode()
             if not file:
                 break
             filename.write(file)
-    elif(parsed[0] == "QUIT"):
+    elif(parsed[0] == "QUIT"):              #IF QUIT then close the connection
         server.close()
     else:
-        print("Invalid Command")
+        print("Invalid Command")            #Catch invalid inputs
 
-host = socket.gethostname()     #Hostname of server
-port = 20                       #Port number for FTP
+host = socket.gethostname()                 #Hostname of server
+port = 20                                   #Port number for FTP
 
-serverSocket = socket.socket()  #instance of socket
-serverSocket.bind((host, port)) #bind host and port
+serverSocket = socket.socket()              #instance of socket
+serverSocket.bind((host, port))             #bind host and port
 
 serverSocket.listen(3)
-while True:
+while True:                                 #Continue listening
     connect, address = serverSocket.accept()
     data = connect.recv(1024).decode()
     _thread.start_new_thread(thread, (serverSocket, connect, str(data)))
